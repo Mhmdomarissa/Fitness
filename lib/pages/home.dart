812cf569337cb2c1e2cc2c5b7c64aa524159ba71
Mhmdomarissa <mhmdomarissa1@gmail.com>
2024-09-11 23:50:17 +0300

    adding the appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: GestureDetector(onTap(){
+
+        },)
+        
+        child Container(
+          width: 30,
+          decoration: BoxDecoration(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/icons/Arrow - left 2.svg',
+              height: 20,
+              width: 20,
+            ),
+            color: Colors(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        actions: [ GestureDetector(
+          onTap(){
+
+          },
+           child : Container(
+            width: 37,
+            decoration: BoxDecoration(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+                height: 20,
+                width: 20,
+              ),
+              color: Colors(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+
+        )
+        
+        ],
+      ),
+    );
   }
 }
